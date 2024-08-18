d31440ebc0d7584f9f016c9c631e529c2d4fb71d
Christopher Morton <sonro@gmx.com>
2024-08-18 13:56:25 +0100

    feat: build system

---
+++ include/tega.h
@@ -0,0 +1,6 @@
+#pragma once
+
+typedef struct TE_Pos {
+    int y;
+    int x;
+} TE_Pos;
---
+++ tests/run.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include <tega.h>
+
+int main() {
+    TE_Pos p = {0, 0};
+    printf("p: %d, %d\n", p.y, p.x);
+}
