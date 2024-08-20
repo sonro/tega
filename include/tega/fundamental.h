03b6201348481c0918b1189e3c1be4ecd9326f3b
Christopher Morton <sonro@gmx.com>
2024-08-20 20:29:34 +0100

    fix: fundamental function singature const usage

--- include/tega/fundamental.h
+++ include/tega/fundamental.h
@@ -64,8 +64,8 @@ TERR_Res TFUN_Bytes_append(TFUN_Bytes *bytes, uint8_t byte);
 void TFUN_Bytes_appendUnsafe(TFUN_Bytes *bytes, uint8_t byte);
 TERR_Res TFUN_Bytes_appendSlice(TFUN_Bytes *bytes, const TFUN_BSlice slice);
 void TFUN_Bytes_appendSliceUnsafe(TFUN_Bytes *bytes, const TFUN_BSlice slice);
-uint8_t TFUN_Bytes_get(TFUN_Bytes *const bytes, uint32_t index);
-TFUN_BSlice TFUN_Bytes_slice(TFUN_Bytes *const bytes, uint32_t start,
+uint8_t TFUN_Bytes_get(const TFUN_Bytes *bytes, uint32_t index);
+TFUN_BSlice TFUN_Bytes_slice(const TFUN_Bytes *bytes, uint32_t start,
                              uint32_t end);
 
 /**
@@ -101,10 +101,10 @@ TERR_Res TFUN_CString_appendSlice(TFUN_CString *string,
                                   const TFUN_CStrSlice slice);
 void TFUN_CString_appendSliceUnsafe(TFUN_CString *string,
                                     const TFUN_CStrSlice slice);
-char TFUN_CString_get(TFUN_CString *const string, uint32_t index);
-TFUN_CStrSlice TFUN_CString_slice(TFUN_CString *const string, uint32_t start,
+char TFUN_CString_get(const TFUN_CString *string, uint32_t index);
+TFUN_CStrSlice TFUN_CString_slice(const TFUN_CString *string, uint32_t start,
                                   uint32_t end);
-TFUN_BSlice TFUN_CString_bSlice(TFUN_CString *const string, uint32_t start,
+TFUN_BSlice TFUN_CString_bSlice(const TFUN_CString *string, uint32_t start,
                                 uint32_t end);
 // Zero copy but `string` will no longer be valid after this call
 TFUN_Bytes TFUN_CString_toBytes(TFUN_CString *string);
@@ -138,10 +138,10 @@ TERR_Res TFUN_WString_appendSlice(TFUN_WString *string,
                                   const TFUN_WStrSlice slice);
 void TFUN_WString_appendSliceUnsafe(TFUN_WString *string,
                                     const TFUN_WStrSlice slice);
-wchar_t TFUN_WString_get(TFUN_WString *const string, uint32_t index);
-TFUN_WStrSlice TFUN_WString_slice(TFUN_WString *const string, uint32_t start,
+wchar_t TFUN_WString_get(const TFUN_WString *string, uint32_t index);
+TFUN_WStrSlice TFUN_WString_slice(const TFUN_WString *string, uint32_t start,
                                   uint32_t end);
-TFUN_BSlice TFUN_WString_bSlice(TFUN_WString *const string, uint32_t start,
+TFUN_BSlice TFUN_WString_bSlice(const TFUN_WString *string, uint32_t start,
                                 uint32_t end);
 // Zero copy but `string` will no longer be valid after this call
 TFUN_Bytes TFUN_WString_toBytes(TFUN_WString *string);
